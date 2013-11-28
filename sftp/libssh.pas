bdc1e179037db14d8335ca070b9550dc935affbc
Chusik <alexx2000@mail.ru>
2013-11-28 09:26:17 +0400

    Some sftp API

--- sftp/libssh.pas
+++ sftp/libssh.pas
@@ -1,6 +1,7 @@
 unit libssh;
 
 {$mode objfpc}{$H+}
+{$packrecords c}
 
 interface
 
@@ -29,13 +30,31 @@ const
   SSH_DISCONNECT_NO_MORE_AUTH_METHODS_AVAILABLE = 14;
   SSH_DISCONNECT_ILLEGAL_USER_NAME              = 15;
 
+  //* Flags for open_ex() */
+  _LIBSSH2_SFTP_OPENFILE                        = 0;
+  _LIBSSH2_SFTP_OPENDIR                         = 1;
+
 type
   //* Session API */
   PLIBSSH2_SESSION = type Pointer;
+  //* SFTP API */
+  PLIBSSH2_SFTP = type Pointer;
+  PLIBSSH2_SFTP_HANDLE = type Pointer;
+  PLIBSSH2_SFTP_ATTRIBUTES = ^LIBSSH2_SFTP_ATTRIBUTES;
+  LIBSSH2_SFTP_ATTRIBUTES = record
+    flags: culong;
+    filesize: cuint64;
+    uid, gid: culong;
+    permissions: culong;
+    atime, mtime: culong;
+  end;
   //* Malloc callbacks */
   LIBSSH2_ALLOC_FUNC = function(count: csize_t; abstract: Pointer): Pointer; cdecl;
   LIBSSH2_REALLOC_FUNC = function(ptr: Pointer; count: csize_t; abstract: Pointer): Pointer; cdecl;
   LIBSSH2_FREE_FUNC = procedure(ptr: Pointer; abstract: Pointer); cdecl;
+  //* Callbacks for special SSH packets */
+  LIBSSH2_PASSWD_CHANGEREQ_FUNC = procedure(session: PLIBSSH2_SESSION; var newpw: PAnsiChar;
+                                            var newpw_len: cint; abstract: Pointer); cdecl;
 
 var
   //* Session API */
@@ -50,9 +69,36 @@ var
                                           const description: PAnsiChar;
                                           const lang: PAnsiChar): cint; cdecl;
   libssh2_session_free: function(session: PLIBSSH2_SESSION): cint; cdecl;
+  libssh2_session_set_blocking: procedure(session: PLIBSSH2_SESSION; blocking: cint); cdecl;
+
+  //* Userauth API */
+  libssh2_userauth_password_ex: function(session: PLIBSSH2_SESSION;
+                                         const username: PAnsiChar;
+                                         username_len: cuint;
+                                         const password: PAnsiChar;
+                                         password_len: cuint;
+                                         passwd_change_cb: LIBSSH2_PASSWD_CHANGEREQ_FUNC): cint; cdecl;
+  //* SFTP API */
+  libssh2_sftp_init: function(session: PLIBSSH2_SESSION): PLIBSSH2_SFTP; cdecl;
+  libssh2_sftp_shutdown: function(sftp: PLIBSSH2_SFTP): cint; cdecl;
+  //* File / Directory Ops */
+  libssh2_sftp_open_ex: function(sftp: PLIBSSH2_SFTP;
+                                 const filename: PAnsiChar;
+                                 filename_len: cint; flags: culong;
+                                 mode: clong; open_type: cint): PLIBSSH2_SFTP_HANDLE; cdecl;
+   libssh2_sftp_readdir_ex: function(handle: PLIBSSH2_SFTP_HANDLE;
+                                     buffer: PAnsiChar; buffer_maxlen: csize_t;
+                                     longentry: PAnsiChar; longentry_maxlen: csize_t;
+                                     attrs: PLIBSSH2_SFTP_ATTRIBUTES): cint; cdecl;
+  libssh2_sftp_close_handle: function(handle: PLIBSSH2_SFTP_HANDLE): cint; cdecl;
 
   function libssh2_session_init: PLIBSSH2_SESSION; inline;
   function libssh2_session_disconnect(session: PLIBSSH2_SESSION; const description: PAnsiChar): cint; inline;
+  function libssh2_userauth_password(session: PLIBSSH2_SESSION; const username: PAnsiChar; const password: PAnsiChar): cint; inline;
+  function libssh2_sftp_open(sftp: PLIBSSH2_SFTP; const filename: PAnsiChar; flags: culong; mode: clong): PLIBSSH2_SFTP_HANDLE; inline;
+  function libssh2_sftp_opendir(sftp: PLIBSSH2_SFTP; const path: PAnsiChar): PLIBSSH2_SFTP_HANDLE; inline;
+  function libssh2_sftp_close(handle: PLIBSSH2_SFTP_HANDLE): cint; inline;
+  function libssh2_sftp_closedir(handle: PLIBSSH2_SFTP_HANDLE): cint; inline;
 
 implementation
 
@@ -65,6 +111,34 @@ function libssh2_session_disconnect(session: PLIBSSH2_SESSION; const description
 begin
   Result:= libssh2_session_disconnect_ex(session, SSH_DISCONNECT_BY_APPLICATION,
                                          description, '');
+end;
+
+function libssh2_userauth_password(session: PLIBSSH2_SESSION;
+  const username: PAnsiChar; const password: PAnsiChar): cint;
+begin
+  Result:= libssh2_userauth_password_ex(session, username, strlen(username),
+                                        password, strlen(password), nil);
+end;
+
+function libssh2_sftp_open(sftp: PLIBSSH2_SFTP; const filename: PAnsiChar;
+  flags: culong; mode: clong): PLIBSSH2_SFTP_HANDLE;
+begin
+  Result:= libssh2_sftp_open_ex(sftp, filename, strlen(filename), flags, mode, _LIBSSH2_SFTP_OPENFILE);
+end;
+
+function libssh2_sftp_opendir(sftp: PLIBSSH2_SFTP; const path: PAnsiChar): PLIBSSH2_SFTP_HANDLE;
+begin
+  Result:= libssh2_sftp_open_ex(sftp, path, strlen(path), 0, 0, _LIBSSH2_SFTP_OPENDIR);
+end;
+
+function libssh2_sftp_close(handle: PLIBSSH2_SFTP_HANDLE): cint;
+begin
+  Result:= libssh2_sftp_close_handle(handle);
+end;
+
+function libssh2_sftp_closedir(handle: PLIBSSH2_SFTP_HANDLE): cint;
+begin
+  Result:= libssh2_sftp_close_handle(handle);
 end;
 
 end.
